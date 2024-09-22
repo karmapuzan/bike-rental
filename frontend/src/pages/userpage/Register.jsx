0d01c23c0d7b87516b449a3c0d8c63ec2aa4ea1d
karmapuzan <karmacharyapujan10@gmail.com>
2024-09-22 20:26:15 +0545

    nav bar

--- frontend/src/pages/userpage/Register.jsx
+++ frontend/src/pages/userpage/Register.jsx
@@ -3,6 +3,8 @@ import UserContainer from '../../component/container/UserContainer'
 import { UserRegister } from '../../actions/UserAction'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import './user.css';
+
 
 const Register = () => {
     const dispatch = useDispatch()
@@ -18,7 +20,7 @@ const Register = () => {
     console.log("registeruser", registeruser)
 
 
-   
+
 
 
     const handleRegister = (e)=>{
@@ -37,35 +39,53 @@ const Register = () => {
 
         }
     },[])
-  return (
+return (
     <UserContainer>
-    <div>
-        <h1>register</h1>
+        <section className='main'>
+    <div className="container" >
+        <h1>User Register</h1>
+        <form className="form" onSubmit={handleRegister}>
+            <div className='input-box'>
+                <label htmlFor="name">name</label>
+                <input type="text" name="name" id="name" value={name} onChange={(e)=>setname(e.target.value)}/>
+            </div>
 
-        <form onSubmit={handleRegister}>
-            <label htmlFor="name">name</label>
-            <input type="text" name="name" id="name" value={name} onChange={(e)=>setname(e.target.value)}/>
+            <div className='input-box'>
             <label htmlFor="email">email</label>
             <input type="email" name="email" id="email" value={email} onChange={(e)=>setemail(e.target.value)}/>
+            </div>
+
+            <div className='input-box'>
             <label htmlFor="address">address</label>
             <input type="text" name="address" id="address" value={address} onChange={(e)=>setaddress(e.target.value)}/>
+            </div>
+
+            <div className='input-box'>
             <label htmlFor="password">password</label>
             <input type="password" name="password" id="password" value={password} onChange={(e)=>setpassword(e.target.value)}/>
+            </div>
+
+            <div className='input-box'>
             <label htmlFor="contact">contact: </label>
             <input type="number" name="contact" id="contact" value={contact} onChange={(e)=>setcontact(e.target.value)}/>
-           
+            </div>         
+            
+            
+            
+    
 
             <button type="submit">register</button>
         </form>
 
         <div className="error-loading">
-           <p>{error && <p>error</p>}</p> 
-           {loading && <p>loading ... </p>}
+        <p>{error && <p>error</p>}</p> 
+        {loading && <p>loading ... </p>}
         </div>
-      
+
     </div>
+    </section>
     </UserContainer>
-  )
+)
 }
 
 export default Register
