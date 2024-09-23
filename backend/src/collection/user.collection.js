5f8152b11279fc8780c5a1fdbd0fb6462b08664c
karmapuzan <karmacharyapujan10@gmail.com>
2024-09-23 17:42:46 +0545

    admion

--- backend/src/collection/user.collection.js
+++ backend/src/collection/user.collection.js
@@ -86,7 +86,7 @@ const registerUser = AsyncHandler(async(req, res)=>{
         
     } catch (error) {
         console.log("error creating user")
-        throw new ApiError(400, "error creating user")
+        throw new ApiError(400, "error creating user", error)
         
     }
 
